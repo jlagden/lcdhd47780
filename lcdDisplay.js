339af20740e666ae27c3ab52e38dd7b0b04c4a12
jlagden <jlagden@binary.org.uk>
2017-05-08 10:51:42 +0100

    Update lcdDisplay.js

--- lcdDisplay.js
+++ lcdDisplay.js
@@ -2,6 +2,7 @@
 
 var COLS = 16;
 var ROWS = 2;
+var SCROLL_SPEED = 300;
 
 var lcd = require('lcd');
 
@@ -10,27 +11,38 @@ module.exports = lcdDisplay;
 function lcdDisplay(context) {
 
 	// Test GPIO pins, eventually get this from configuration
-	self.lcd = new Lcd({rs: 7, e: 8, data: [25, 24, 23, 18], cols: 16, rows: 2});
+	this.lcd = new Lcd({rs: 7, e: 8, data: [25, 24, 23, 18], cols: 16, rows: 2});
 
 }
 
 lcdDisplay.prototype.displayTrackInfo = function(data,pos) {
 
 	pos = pos || 0;
+	var trackInfo = data.artist + '-' + data.title;
+	var duration = data.duration;
 	
-	if(pos === str.length){
-		pos = 0;
+	if (trackInfo.length > COLS) {
+	} else { // If the length is les then the display width, we just need to display it and forget about scrolling
+		// Add spaces to fill up the rest of the display
+		trackInfo = trackInfo + (' ').repeat(COLS-trackInfo.length);
 	}
 	
-	lcd.print(data[pos],function (err) {
+	this.lcd.setCursor(0,0);
+	this.lcd.print(trackInfo.substr(pos,COLS),function (err) {
 		if (err) {
 			throw err;
 		}
-		
-		setTimeout(function () {
-			this.displayTrackInfo(str, pos + 1);
-		},300);
-	};
+		// Track info printed ok, set lets print elapsed / duration
+		this.lcd.setCursor(0,1);
+		this.lcd.print(this._formatSeekDuration(,duration),function (err) {
+			if (err) {
+				throw err;
+			}
+			setTimeout(function () {
+				this.displayTrackInfo(str, pos + 1);
+			},SCROLL_SPEED);
+		}
+	});
 
 }
 
