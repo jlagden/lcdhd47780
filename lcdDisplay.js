65bcd740d0feb5b302fc393fd3cd21baa333d6d7
jlagden <jlagden@binary.org.uk>
2017-06-03 20:58:46 +0100

    Update lcdDisplay.js

--- lcdDisplay.js
+++ lcdDisplay.js
@@ -9,6 +9,7 @@ var Lcd = require('lcd');
 module.exports = lcdDisplay;
 
 function lcdDisplay(context) {
+  
 	var self = this;
 	
 	self.displayTimer = undefined;
@@ -20,17 +21,21 @@ function lcdDisplay(context) {
 
 	// Test GPIO pins, eventually get this from configuration
 	self.lcd = new Lcd({rs: 8, e: 7, data: [25, 24, 23, 18], cols: 16, rows: 2});
-};
+  
+}
 
 lcdDisplay.prototype.close = function() {
+  
 	var self = this;
 	if (self.displayTimer !== undefined) {
 		clearTimeout(self.displayTimer);
 	}
 	self.lcd.close();
+  
 };
 
 lcdDisplay.prototype.endOfSong = function() {
+  
 	var self = this;
 
 	if (self.displayTimer !== undefined) {
@@ -38,12 +43,12 @@ lcdDisplay.prototype.endOfSong = function() {
 		self.displayTimer = undefined;
 	}	
 	self.lcd.clear();
-}
+  
+};
 
 lcdDisplay.prototype.pushState = function(state)  {
-	var self = this;
-
-	// Received pushState event
+	
+  var self = this;
 	self.elapsed = state.seek;
 	if (state.status === 'play') {		
 		if (self._needStartDisplayInfo(state)) { // Clear the timeout and start displayInfo again
@@ -61,10 +66,12 @@ lcdDisplay.prototype.pushState = function(state)  {
 		self.elapsed = state.seek; // Update elapsed
 	}
 	self.currentState = state; // Update state
-}
+  
+};
 
 lcdDisplay.prototype.displayTrackInfo = function(data,pos) {
-	var self = this; 
+	
+  var self = this; 
 	var duration = data.duration;
 	
 	if (self.elapsed >= duration * 1000) {
@@ -73,7 +80,7 @@ lcdDisplay.prototype.displayTrackInfo = function(data,pos) {
 	
 		var trackInfo = data.artist + '-' + data.title;
 
-if (trackInfo.length > COLS) {
+  	if (trackInfo.length > COLS) {
 			// Piece the string together in such a way so it constantly scrolling
 			trackInfo = trackInfo + '          ' + trackInfo.substr(0, COLS);
 		} else { // If the length is les then the display width, we just need to display it and forget about scrolling
@@ -89,19 +96,20 @@ if (trackInfo.length > COLS) {
 		self.lcd.setCursor(0,0);
 		// Print track info
 		self.lcd.print(trackInfo.substr(pos,COLS),function (err) {
-			// Track info printed ok, set lets print elapsed / duration
+			
+      // Track info printed ok, set lets print elapsed / duration
 			self.lcd.setCursor(0,1);
 			self.lcd.print(self._formatSeekDuration(self.elapsed,duration),function (err) {
 				self.displayTimer = setTimeout(function () {
-					if (self.currentState.status != 'pause')
-	  	    				self.elapsed += SCROLL_SPEED;
-					self.displayTrackInfo(data, pos + 1);
+				if (self.currentState.status != 'pause')
+	  	    self.elapsed += SCROLL_SPEED;
+				self.displayTrackInfo(data, pos + 1);
 				},SCROLL_SPEED);
 			});
 		});
 	}
 
-}
+};
 
 // If we have started playing or the artist/track has changed we need
 // to restart displayInfo		 
@@ -109,14 +117,14 @@ if (trackInfo.length > COLS) {
 lcdDisplay.prototype._needStartDisplayInfo = function(state) {
 
 	var self = this;
-  	return  ((state.status === 'play' && self.currentState.status === 'stop') ||
+  return  ((state.status === 'play' && self.currentState.status === 'stop') ||
         	self.currentState.artist !== state.artist || 
-  	  	  	self.currentState.title !== state.title);
-}
+  	  	  self.currentState.title !== state.title);
+  
+};
 		       
 lcdDisplay.prototype._formatSeekDuration = function(seek, duration) { 
 
-	var self = this; 
 	var seekSec = Math.ceil(seek / 1000).toFixed(0); 
 	var seekMin = Math.floor(seekSec / 60); 
 	seekSec = seekSec - seekMin * 60; 
@@ -130,9 +138,9 @@ lcdDisplay.prototype._formatSeekDuration = function(seek, duration) {
 	if (seekMin < 10) (seekMin = "0" + seekMin);
 	if (seekSec < 10) (seekSec = "0" + seekSec);
 	if (durMin < 10)  (durMin = "0" + durMin);
-  	if (durSec < 10)  (durSec = "0" + durSec);  
+  if (durSec < 10)  (durSec = "0" + durSec);  
 	
- 	 var txt = seekMin+":"+seekSec+"/"+durMin+":"+durSec;
+ 	var txt = seekMin + ":" + seekSec + "/" + durMin + ":" + durSec;
  
 	return txt.substr(0,COLS); 
 
