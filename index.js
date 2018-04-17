adb7316e113476c7c7b1be83beab9164683be95c
jlagden <jlagden@binary.org.uk>
2018-04-17 10:36:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -58,6 +58,46 @@ ControllerlcdHD47780.prototype.onStop = function() {
 	
 }
 
+ControllerExamplePlugin.prototype.onRestart = function() {
+
+    var self = this;
+    // Optional, use if you need it
+
+};
+
+// Configuration Methods -----------------------------------------------------------------------------
+
+ControllerExamplePlugin.prototype.getUIConfig = function() {
+
+    var defer = libQ.defer();
+    var self = this;
+
+    var lang_code = this.commandRouter.sharedVars.get('language_code');
+
+    self.commandRouter.i18nJson(__dirname+'/i18n/strings_'+lang_code+'.json',
+        __dirname+'/i18n/strings_en.json',
+        __dirname + '/UIConfig.json')
+		
+	.then(function(uiconf) {
+
+		uiconf.sections[0].content[0].value = self.config.get('RS');
+			uiconf.sections[0].content[1].value = self.config.get('E');
+			uiconf.sections[0].content[2].value = self.config.get('D4');
+			uiconf.sections[0].content[3].value = self.config.get('D5');
+			uiconf.sections[0].content[4].value = self.config.get('D6');
+            uiconf.sections[0].content[5].value = self.config.get('D7');
+		defer.resolve(uiconf);
+
+	})
+	.fail(function() {
+
+		defer.reject(new Error());
+
+	});
+
+    return defer.promise;
+
+};
 
 ControllerlcdHD47780.prototype.getConf = function(varName) {
 
@@ -67,44 +107,6 @@ ControllerlcdHD47780.prototype.getConf = function(varName) {
 	return libQ.resolve();
 
 };
-
-
-ControllerlcdHD47780.prototype.getConfigurationFiles = function() {
-	return ['config.json'];
-}
-
-ControllerlcdHD47780.prototype.getUIConfig = function() {
-	
-	var defer = libQ.defer();
-	var self = this;
-	
-	var lang_code = self.commandRouter.sharedVars.get('language_code');
-
-        self.commandRouter.i18nJson(__dirname+'/i18n/strings_'+lang_code+'.json',
-									__dirname+'/i18n/strings_en.json',
-									__dirname + '/UIConfig.json')
-
-        .then(function(uiconf) {
-		
-			uiconf.sections[0].content[0].value = self.config.get('RS');
-			uiconf.sections[0].content[1].value = self.config.get('E');
-			uiconf.sections[0].content[2].value = self.config.get('D4');
-			uiconf.sections[0].content[3].value = self.config.get('D5');
-			uiconf.sections[0].content[4].value = self.config.get('D6');
-            uiconf.sections[0].content[5].value = self.config.get('D7');
-			
-            defer.resolve(uiconf);
-
-        })
-        .fail(function(){
-
-            defer.reject(new Error());
-
-        });
-	
-	return defer.promise;
-
-}
 
 ControllerlcdHD47780.prototype.saveGPIOConfig = function(data) {
 	
@@ -121,13 +123,3 @@ ControllerlcdHD47780.prototype.setUIConfig = function(data) {
       return libQ.resolve();
 
 };
-
-ControllerlcdHD47780.prototype.pushState = function(state) {
-	
-	var self = this;
-	self.lcdDisplay.pushState(state);
-	return libQ.resolve();
-
-}
-
-// Public Methods
